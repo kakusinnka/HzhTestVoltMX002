94dd57c81dfa1bd9b16947112dcc663e9b4ed142
haozhenhua <1305510482@qq.com>
2024-07-14 00:05:33 +0900

    update

--- controllers/mobile/TestForm009Controller/TestForm009Controller.js
+++ controllers/mobile/TestForm009Controller/TestForm009Controller.js
@@ -1,17 +1,23 @@
 define({
 
-  //Type your controller code here 
+  //Type your controller code here ,
   confirmationAlert: function () {
     //Creating the basicConfig object 
     var basicConf = {
       message: "This is an confirmation alert",
       alertType: constants.ALERT_TYPE_CONFIRMATION,
-
+      alertTitle: "Confirmation",
+      noLabel: "NoNo",
+      yesLabel: "YesYes",
+      // alertIcon: "acme.png",
     };
     //Creating the pspConfig object
     var pspConfig = {
       "contentAlignment": constants.ALERT_CONTENT_ALIGN_CENTER
     };
-    kony.ui.Alert(basicConf, pspConfig);
-  }
+    var myAlert = kony.ui.Alert(basicConf, pspConfig);
+    
+    // voltmx.ui.dismissAlert(myalert);
+  } 
+  
 });
