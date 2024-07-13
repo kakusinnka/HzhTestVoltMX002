bc1e6230f51a50c4553d6c3245e94306ba2122ed
haozhenhua <1305510482@qq.com>
2024-07-13 14:46:40 +0900

    button add onClick event

--- controllers/mobile/TestForm009Controller/TestForm009Controller.js
+++ controllers/mobile/TestForm009Controller/TestForm009Controller.js
@@ -1,5 +1,17 @@
-define({ 
+define({
 
- //Type your controller code here 
+  //Type your controller code here 
+  confirmationAlert: function () {
+    //Creating the basicConfig object 
+    var basicConf = {
+      message: "This is an confirmation alert",
+      alertType: constants.ALERT_TYPE_CONFIRMATION,
 
- });
+    };
+    //Creating the pspConfig object
+    var pspConfig = {
+      "contentAlignment": constants.ALERT_CONTENT_ALIGN_CENTER
+    };
+    kony.ui.Alert(basicConf, pspConfig);
+  }
+});
